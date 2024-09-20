f5047369106c3c5e50325754b16e3405200ebf18
Ghulam Mustafa <157616503+G-Mustafa1@users.noreply.github.com>
2024-09-21 03:28:53 +0500

    Update app.js

--- studentPortal/app.js
+++ studentPortal/app.js
@@ -36,10 +36,8 @@ const signOutButton = document.getElementById("sign-out");
             text: "User signed out successfully.",
             icon: "success"
          });
-         // await setTimeout(()=>{
            window.location.href = "../login/index.html";     
            
-         // },3000)     
    }catch (error) {
       const errorMessage = error.message;
       Swal.fire({
